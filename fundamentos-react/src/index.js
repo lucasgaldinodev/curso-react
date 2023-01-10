b096ce0baefbac4eeafbbe9f09aa60d9098a10db
Lucas Galdino <lucasgaldino7@gmail.com>
2023-01-10 10:06:49 -0300

    10/01/2023

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
+
+ReactDOM.render(
+  <div id="app">
+    <Primeiro />
+    <ComParametro titulo="Situação do Aluno" aluno="Pedro Silva" nota="9.3" />
+    <Fragmento />
+  </div>,
+  document.getElementById('root'),
+)
